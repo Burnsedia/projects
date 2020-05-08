b4c93b38f6dbe817753e2e787032e1a064cddf92
burnsedia <bburnsed1995@gmail.com>
2020-05-07 22:55:08 -0400

    renamed main to unit4 and added test.py

---
+++ jernal/unit4/unit4.py
@@ -0,0 +1,28 @@
+from math import sqrt
+
+def hypotenuse(a,b):
+    c_to_the_2 = a**2 + b**2
+
+    print(c_to_the_2**.5)
+    return c_to_the_2**.5
+
+
+# cf is curection factor
+# sl is sugar leval in mg/dl
+def blood_sugar_currection(cf,sl):
+    resalt = 0
+    if sl <= 100:
+        resalt = (sl - 100)/cf
+    else:
+        resalt = (sl - 100)/cf
+    print(resalt)
+    return resalt
+
+hypotenuse(3,4)
+hypotenuse(4,5)
+hypotenuse(6,7)
+
+blood_sugar_currection(40,300)
+blood_sugar_currection(40,200)
+blood_sugar_currection(40,100)
+blood_sugar_currection(40,80)
