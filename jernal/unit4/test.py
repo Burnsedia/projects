a1cbc66eacbe1c76950333880bfabcd1119a769e
burnsedia <bburnsed1995@gmail.com>
2020-05-07 22:57:39 -0400

    starting unit test for TTD

---
+++ jernal/unit4/test.py
@@ -0,0 +1,11 @@
+from unit4 import hypotenuse
+from unit4 import blood_sugar_currection
+
+hypotenuse(3,4)
+hypotenuse(4,5)
+hypotenuse(6,7)
+
+blood_sugar_currection(40,300)
+blood_sugar_currection(40,200)
+blood_sugar_currection(40,100)
+blood_sugar_currection(40,80)
